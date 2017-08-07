68f68aae461ebec5cf74a82a74f30d16030fb0a1
Your Name <you@example.com>
2017-08-07 11:26:56 +0700

    cek file controller

--- app/Core/Route.php
+++ app/Core/Route.php
@@ -2,12 +2,21 @@
 
 class Route
 {
+  protected $controller = 'HomeController';
+  protected $method = 'index';
+  protected $param = [];
   public function __construct()
   {
     if(isset($_GET['url']))
     {
       $url = explode('/', filter_var(trim($_GET['url']), FILTER_SANITIZE_URL));
-      print_r($url);
     }
+      $url[0] = $url[0] .'Controller.php'
+      if(file_exists($url[0]))
+      {
+        die('exist!');
+      }else{
+          die('not exist!');
+      }
   }
 }
